9cbfdb5e87f7d1fbd2c98db933e08495049aaef8
Jesse-WJ <m17610397795@163.com>
2023-04-13 20:24:15 +0800

    add multi function

--- src/ComputeFactory.java
+++ src/ComputeFactory.java
@@ -1,8 +1,4 @@
 public class ComputeFactory {
-
-    private static final Add add = new Add();
-    private static final Sub sub = new Sub();
-
     public static Computable getCompute(String symbol)
         throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         return (Computable) Class.forName(symbol).newInstance();
